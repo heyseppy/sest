53a1d76322d532ddb93fd04e8af8c5a653610178
heyseppy <sepkimiaei1@gmail.com>
2021-02-05 12:24:18 +0800

    added 'get' compatibility and error handling

--- sest!/Form1.Designer.cs
+++ sest!/Form1.Designer.cs
@@ -29,67 +29,71 @@ namespace sest_
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.url_txt = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.field_val_lbx = new System.Windows.Forms.ListBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.value_txt = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.field_txt = new System.Windows.Forms.TextBox();
+            this.get_btn = new System.Windows.Forms.Button();
+            this.post_btn = new System.Windows.Forms.Button();
+            this.put_btn = new System.Windows.Forms.Button();
+            this.results_txt = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // url_txt
             // 
-            this.textBox1.Location = new System.Drawing.Point(55, 14);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(809, 43);
-            this.textBox1.TabIndex = 0;
+            this.url_txt.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.url_txt.Location = new System.Drawing.Point(55, 12);
+            this.url_txt.Multiline = true;
+            this.url_txt.Name = "url_txt";
+            this.url_txt.Size = new System.Drawing.Size(809, 34);
+            this.url_txt.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 33);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(7, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 24);
+            this.label1.Size = new System.Drawing.Size(46, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "url:";
             // 
-            // listBox1
+            // field_val_lbx
             // 
-            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(12, 181);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(852, 68);
-            this.listBox1.TabIndex = 3;
+            this.field_val_lbx.BackColor = System.Drawing.Color.White;
+            this.field_val_lbx.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.field_val_lbx.FormattingEnabled = true;
+            this.field_val_lbx.ItemHeight = 28;
+            this.field_val_lbx.Location = new System.Drawing.Point(12, 181);
+            this.field_val_lbx.Name = "field_val_lbx";
+            this.field_val_lbx.Size = new System.Drawing.Size(852, 60);
+            this.field_val_lbx.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(350, 145);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(350, 140);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 24);
             this.label3.TabIndex = 7;
             this.label3.Text = "value:";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // textBox3
+            // value_txt
             // 
-            this.textBox3.Location = new System.Drawing.Point(422, 134);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(279, 32);
-            this.textBox3.TabIndex = 6;
+            this.value_txt.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.value_txt.Location = new System.Drawing.Point(422, 134);
+            this.value_txt.Multiline = true;
+            this.value_txt.Name = "value_txt";
+            this.value_txt.Size = new System.Drawing.Size(279, 32);
+            this.value_txt.TabIndex = 6;
             // 
             // button1
             // 
@@ -106,87 +110,95 @@ namespace sest_
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 142);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(7, 141);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 24);
+            this.label2.Size = new System.Drawing.Size(64, 25);
             this.label2.TabIndex = 10;
             this.label2.Text = "field:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox2
+            // field_txt
             // 
-            this.textBox2.Location = new System.Drawing.Point(74, 134);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(270, 32);
-            this.textBox2.TabIndex = 9;
+            this.field_txt.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.field_txt.Location = new System.Drawing.Point(74, 134);
+            this.field_txt.Multiline = true;
+            this.field_txt.Name = "field_txt";
+            this.field_txt.Size = new System.Drawing.Size(270, 32);
+            this.field_txt.TabIndex = 9;
             // 
-            // button2
+            // get_btn
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(285, 63);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 49);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "GET";
-            this.button2.UseVisualStyleBackColor = false;
+            this.get_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.get_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.get_btn.ForeColor = System.Drawing.Color.Black;
+            this.get_btn.Location = new System.Drawing.Point(285, 63);
+            this.get_btn.Name = "get_btn";
+            this.get_btn.Size = new System.Drawing.Size(107, 49);
+            this.get_btn.TabIndex = 11;
+            this.get_btn.Text = "GET";
+            this.get_btn.UseVisualStyleBackColor = false;
+            this.get_btn.Click += new System.EventHandler(this.get_btn_Click);
             // 
-            // button3
+            // post_btn
             // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(398, 64);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(107, 49);
-            this.button3.TabIndex = 12;
-            this.button3.Text = "POST";
-            this.button3.UseVisualStyleBackColor = false;
+            this.post_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.post_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.post_btn.Location = new System.Drawing.Point(398, 64);
+            this.post_btn.Name = "post_btn";
+            this.post_btn.Size = new System.Drawing.Size(107, 49);
+            this.post_btn.TabIndex = 12;
+            this.post_btn.Text = "POST";
+            this.post_btn.UseVisualStyleBackColor = false;
+            this.post_btn.Click += new System.EventHandler(this.post_btn_Click);
             // 
-            // button4
+            // put_btn
             // 
-            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(511, 64);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(107, 49);
-            this.button4.TabIndex = 13;
-            this.button4.Text = "PUT";
-            this.button4.UseVisualStyleBackColor = false;
+            this.put_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.put_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.put_btn.Location = new System.Drawing.Point(511, 64);
+            this.put_btn.Name = "put_btn";
+            this.put_btn.Size = new System.Drawing.Size(107, 49);
+            this.put_btn.TabIndex = 13;
+            this.put_btn.Text = "PUT";
+            this.put_btn.UseVisualStyleBackColor = false;
+            this.put_btn.Click += new System.EventHandler(this.put_btn_Click);
             // 
-            // textBox4
+            // results_txt
             // 
-            this.textBox4.BackColor = System.Drawing.Color.LightGoldenrodYellow;
-            this.textBox4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(12, 274);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(852, 305);
-            this.textBox4.TabIndex = 14;
+            this.results_txt.BackColor = System.Drawing.Color.White;
+            this.results_txt.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.results_txt.Location = new System.Drawing.Point(12, 247);
+            this.results_txt.Multiline = true;
+            this.results_txt.Name = "results_txt";
+            this.results_txt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.results_txt.Size = new System.Drawing.Size(852, 332);
+            this.results_txt.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(881, 598);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(881, 613);
+            this.Controls.Add(this.results_txt);
+            this.Controls.Add(this.put_btn);
+            this.Controls.Add(this.post_btn);
+            this.Controls.Add(this.get_btn);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.field_txt);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.value_txt);
+            this.Controls.Add(this.field_val_lbx);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Controls.Add(this.url_txt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "sest!";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,18 +206,18 @@ namespace sest_
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox url_txt;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox field_val_lbx;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox value_txt;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox field_txt;
+        private System.Windows.Forms.Button get_btn;
+        private System.Windows.Forms.Button post_btn;
+        private System.Windows.Forms.Button put_btn;
+        private System.Windows.Forms.TextBox results_txt;
     }
 }
 
--- sest!/Form1.cs
+++ sest!/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Net.Http;
+    
 namespace sest_
 {
     public partial class Form1 : Form
@@ -30,6 +31,52 @@ namespace sest_
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(884, 127);
+        }
+
+        private void put_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void post_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void get_btn_Click(object sender, EventArgs e)
+        {
+            if (url_txt.Text.Length > 0)
+            {
+                try
+                {
+                    HttpClient httpClient = new HttpClient();
+                    var result = httpClient.GetAsync(url_txt.Text).Result;
+                    this.Size = new Size(884,604);
+                    field_txt.Enabled = false;
+                    value_txt.Enabled = false;
+                    results_txt.Text = result.ToString();
+                }
+                catch (HttpRequestException ef)
+                {
+                   MessageBox.Show("Exception Caught!");
+                   MessageBox.Show("Message :{0} ", ef.Message);
+                }
+                catch(InvalidOperationException efg)
+                {
+                    MessageBox.Show("Must be a complete url (http/s) included");
+                }
+
+               
+            }
+            else
+            {
+                MessageBox.Show("Please type in an url");
+            }
         }
     }
 }
